321fef1f463a49bf864cf4b2e5b8fa093f074292
shayanabdullah <tuhinafroza07@gmail.com>
2025-10-29 03:04:00 +0600

    test

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -28,41 +28,43 @@ const Review = () => {
         );
     };
 
-  const settings = {
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-      centerMode: true,
+
+const settings = {
+  speed: 500,
+  infinite: true,
+  centerMode: true,
   nextArrow: <NextArrow />,
   prevArrow: <PrevArrow />,
-    initialSlide: 0,
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-        }
+  responsive: [
+    {
+      breakpoint: 10000,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        centerMode: true,
       },
-      {
-        breakpoint: 700,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1
-        }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        centerMode: false,
       },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  };
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false,
+        arrows: false,
+      },
+    },
+  ],
+};
+     
 
 
     return (
