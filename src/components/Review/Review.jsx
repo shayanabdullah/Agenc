14507dd18f4c7a72711cef71691b17a5505261e9
shayanabdullah <tuhinafroza07@gmail.com>
2025-10-29 03:01:33 +0600

    test

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -28,35 +28,42 @@ const Review = () => {
         );
     };
 
-    const settings = {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        speed: 500,
-        initialSlide: 0,
-        centerMode: true,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        initialSlide: 0,
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+      centerMode: true,
+  nextArrow: <NextArrow />,
+  prevArrow: <PrevArrow />,
+    initialSlide: 0,
     responsive: [
-  {
-    breakpoint: 1250,
-    settings: {
-      slidesToShow: 2,
-       infinite : true
-    
-    },
-  },
-  {
-    breakpoint: 824,
-    settings: {
-      slidesToShow: 1,
-      infinite : true
- 
-    },
-  },
-],
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  };
 
-    };
 
     return (
         <>
